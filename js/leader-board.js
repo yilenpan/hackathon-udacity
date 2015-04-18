ab26e7a811114d28e67e00bc48bc6d20ec6f5c6b
Yilen <yilen.pan@gmail.com>
2015-04-18 11:43:06 -0700

    first commit

---
+++ js/leader-board.js
@@ -0,0 +1,57 @@
+var app = angular.module('leaderboard', ['firebase']);
+
+app.constant('FIREBASE_URI', 'http://blinding-heat-8369.firebaseio.com/');
+
+app.controller('MainCtrl', function (ContestantsService) {
+    var main = this;
+    main.newContestant = {lane: '', name: '', score: ''};
+    main.currentContestant = null;
+    main.contestants = ContestantsService.getContestants();
+
+    main.addContestant = function () {
+        ContestantsService.addContestant(angular.copy(main.newContestant));
+        main.newContestant = {lane: '', name: '', score: ''};
+    };
+
+    main.updateContestant = function (contestant) {
+        ContestantsService.updateContestant(contestant);
+    };
+
+    main.removeContestant = function (contestant) {
+        ContestantsService.removeContestant(contestant);
+    };
+
+    main.incrementScore = function () {
+        main.currentContestant.score = parseInt(main.currentContestant.score, 10) + 1;
+        main.updateContestant(main.currentContestant);
+    };
+
+    main.decrementScore = function () {
+        main.currentContestant.score = parseInt(main.currentContestant.score, 10) - 1;
+        main.updateContestant(main.currentContestant);
+    };
+});
+
+app.service('ContestantsService', function ($firebaseArray, FIREBASE_URI) {
+    var service = this;
+    //set ref to a new Firebase object
+    var ref = new Firebase(FIREBASE_URI);
+    //set contestants to the synched array
+    var contestants = $firebaseArray(ref);
+
+    service.getContestants = function () {
+        return contestants;
+    };
+
+    service.addContestant = function (contestant) {
+        contestants.$add(contestant);
+    };
+
+    service.updateContestant = function (contestant) {
+        contestants.$save(contestant);
+    };
+
+    service.removeContestant = function (contestant) {
+        contestants.$remove(contestant);
+    };
+});
